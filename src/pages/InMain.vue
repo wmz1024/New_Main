08859b349c652377392972be25cc255c315496f6
Wmz1024 <i@w03.cc>
2022-08-11 16:05:24 +0800

    u

--- src/pages/InMain.vue
+++ src/pages/InMain.vue
@@ -1,5 +1,36 @@
 <template>
 <div>
-    <h1>114514</h1>
+    <center>
+  <h1>关于我</h1>
+  <p> <img src="https://me.w03.cc" alt="myself"></p></center>
+<h2 id="关于我自己"><center>Hello! This is Wmz1024</center></h2>
+<center>
+   <div id="cardinfo">
+<div class="mdui-card card1">
+        <div class="mdui-card-media">
+          <img src="https://afdian.net/static/img/logo/logo.png">
+          <div class="mdui-card-media-covered mdui-card-media-covered-transparent">
+            <div class="mdui-card-primary">
+              <div class="mdui-card-primary-title" style="color: grey;">爱发电</div>
+            </div>
+          </div>
+        </div>
+        <div class="mdui-card-actions">
+          <a href="https://afdian.net/@wmz1024"><button class="mdui-btn mdui-ripple" style="text-align: center;">进入</button></a>
+        </div>
+      </div>
+      <div class="mdui-card card1">
+        <div class="mdui-card-media">
+          <img src="https://me.w03.cc/">
+          <div class="mdui-card-media-covered mdui-card-media-covered-transparent">
+            <div class="mdui-card-primary">
+              <div class="mdui-card-primary-title" style="color: grey;">博客</div>
+            </div>
+          </div>
+        </div>
+        <div class="mdui-card-actions">
+          <a href="https://blog.w03.cc"><button class="mdui-btn mdui-ripple" style="text-align: center;">进入</button></a>
+        </div>
+      </div></div></center>
 </div>
 </template>
