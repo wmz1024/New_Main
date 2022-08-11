35799a8bad1ba273d42df404f136c172bf10abad
Wmz1024 <i@w03.cc>
2022-08-11 15:55:41 +0800

    u

--- src/pages/InAbout.vue
+++ src/pages/InAbout.vue
@@ -1,3 +1,40 @@
 <template>
-<h1>about</h1>
+<center>
+  <h1>关于我</h1>
+  <p> <img src="https://me.w03.cc" alt="myself"></p>
+<h2 id="关于我自己"><center>关于我自己</center></h2>
+<p>目前现任</p>
+<p>学生</p>
+<p>会点python</p>
+<p>会点html css js</p>
+<p>会点vue react</p>
+<p>什么都想要免费的（不是）</p>
+<h3 id="🔴-目前使用">🔴 目前使用</h3>
+<p>使用hexo框架来驱动博客</p>
+<p>使用butterfly主题来美化博客</p>
+<p>使用leancloud来驱动artitalk说说和Waline评论</p>
+<p>使用github和vercel的应用系统</p>
+<p>使用<a href="https://thiscatdoesnotexist.com/">https://thiscatdoesnotexist.com/</a> 喵图服务</p>
+<p>由又拍云存储一些图片</p>
+<p>本站一些图片存储在 gitee u3 节点 v3 节点</p>
+<p>  gitee存储较多（多数为文章图片）</p>
+<p>  u3节点存储网站图片</p>
+<p>  v3节点暂时无法访问</p>
+<p>由Wmz1024运营</p>
+<p>感谢每个人</p>
+<h3 id="🟠-自己有的一些电子设备（可能吧）">🟠 自己有的一些电子设备（可能吧）</h3>
+<p>手里有一台笔记本 很垃圾</p>
+<p>手机有个iPhone 5s（非常垃圾）还有一个类似手机的平板（像是八英寸的手机）</p>
+<p>一台华为的nova3</p>
+<p>查看报告</p>
+<p><a href="https://blog.w03.cc/c-re.html">https://blog.wmz.link/c-re.html</a> 这个可能是硬件信息</p>
+<p><a href="https://blog.w03.cc/y-re.html">https://blog.wmz.link/y-re.html</a> 这个可能是性能测试</p>
+<p><a href="https://blog.w03.cck/pcre.html">https://blog.wmz.link/pcre.html</a> 这个是cpuz的报告文件</p>
+<h3 id="🟢关于我自己">🟢关于我自己</h3>
+<p>我是Wmz1024 可以叫我mz之类的</p>
+<p>是一个小学学生</p>
+<p>正在学python</p>
+<p><span class="mask" title="害">我认为普通人不需要介绍 所以以前都没写</span></p>
+
+</center>
 </template>
