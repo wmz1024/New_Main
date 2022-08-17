331d9cb93ed30beac08a3801258e6b69dd476d4c
Wmz1024 <82153828+wmz1024@users.noreply.github.com>
2022-08-17 13:03:50 +0800

    u

---
+++ src/pages/InGet-002.vue
@@ -0,0 +1,6 @@
+<template>
+<h1>下载页面</h1>
+<p><a href='https://pan.w03.cc/s/wVin'>Wpan</a></p>
+<p><a href='https://u1.877771.xyz/wfile/share/mods(%E4%B8%8A%E4%BC%A0%E6%97%B6%E9%97%B4%202022.8.17.zip'>OneDrive直链</a></p>
+<p><a href='https://wmz1024.lanzouj.com/iLr1O09p0c2j'>蓝奏云</a></p>
+</template>
